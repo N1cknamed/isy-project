24d3e4488e5b90fbc54fa81db5adce07200bc995
martprenger <mart.prenger@gmail.com>
2023-11-23 11:56:11 +0100

    added server to gitigonre so i can put the server jar in de same folder as the project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,9 +10,9 @@ public class Main {
     public static void main(String[] args) {
 
         // login page
-        Login.launch(Login.class, args);
-        String enteredText = Login.getEnteredText();
-        System.out.println("Entered Text: " + enteredText);
+//        Login.launch(Login.class, args);
+//        String enteredText = Login.getEnteredText();
+//        System.out.println("Entered Text: " + enteredText);
 
         // tic tac toe gui (can choose to play against ai) TODO: reset game after switching game type
 //        TictactoeGui.launch(TictactoeGui.class, args);
@@ -30,23 +30,23 @@ public class Main {
         to start the server in cmd type: java -jar newgameserver-1.0.jar
         then get the ip and port of the server and type those in the appropriate inputs
         */
-//        ServerController x = new ServerController("localhost", 7789);
-//        Message message = x.getMessage();
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        while (true) {
-//            String userInput = scanner.nextLine();
-//            x.sendMessage(userInput);
-//            synchronized (message) {
-//                try {
-//                    message.wait();
-//                    System.out.println(message.getMessage());
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
-//        }
+        ServerController x = new ServerController("localhost", 7789);
+        Message message = x.getMessage();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            x.sendMessage(userInput);
+            synchronized (message) {
+                try {
+                    message.wait();
+                    System.out.println(message.getMessage());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
 
     }
 }
