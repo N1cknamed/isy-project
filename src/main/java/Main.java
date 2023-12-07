b17f9544d033322a2cf70a92871539986097f784
Faizo Hassan <38426116+izab662@users.noreply.github.com>
2023-12-07 10:35:46 +0100

    updated Main.java
    HomeGui launch added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,44 +9,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // login page
-        Login.launch(Login.class, args);
-        String enteredText = Login.getEnteredText();
-        System.out.println("Entered Text: " + enteredText);
-
         // tic tac toe gui (can choose to play against ai) TODO: reset game after switching game type
 //        TictactoeGui.launch(TictactoeGui.class, args);
+        HomeGui.launch(HomeGui.class,args);
 
-        // tic tac toe ai cli (x is player o is ai)
-//        Tictactoe game = new Tictactoe();
-//        game.playGame();
-
-        // Battleships
-//        BattleShips game = new BattleShips();
-//        game.printBoard();
-        
-        // server
-        /*
-        to start the server in cmd type: java -jar newgameserver-1.0.jar
-        then get the ip and port of the server and type those in the appropriate inputs
-        */
-//        ServerController x = new ServerController("localhost", 7789);
-//        Message message = x.getMessage();
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        while (true) {
-//            String userInput = scanner.nextLine();
-//            x.sendMessage(userInput);
-//            synchronized (message) {
-//                try {
-//                    message.wait();
-//                    System.out.println(message.getMessage());
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
-//        }
 
     }
 }
